1ac4536b3e01b7e72607f581ea7fec414da3f911
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-13 22:31:18 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -19,53 +19,82 @@ typedef struct{
 } Data_sampel;
 
 Data_sampel* open_file(Data_sampel* data, int* jml_sampel){
-    FILE* seq_file;
-    char namaFile[10]; //asumsi maksimal panjang nama file 7
-    char line[255]; 
-    char* token;
-    int n=0;
+	FILE* seq_file;
+	char namaFile[10]; //asumsi maksimal panjang nama file 7
+	char line[255]; 
+	char* token;
+	int n=0;
 
-    printf("Masukkan nama file: ");
-    scanf("%s", &namaFile);
+	printf("Masukkan nama file: ");
+	scanf("%s", &namaFile);
 
-    seq_file=fopen(namaFile, "r");
+	seq_file=fopen(namaFile, "r");
 
-    while (fgets(line, 81, seq_file)){
-        if(n>0){
-            data=...;
-        }
-        token = strtok(line,",");
-        (data+n)->sekuens = ...;
-        strcpy((data+n)->sekuens,token);
+	while (fgets(line, 81, seq_file)){
+		if(n>0){
+			data=realloc(data,(n+1)*sizeof(Data_sampel));
+		}
+		token = strtok(line,",");
+		(data+n)->sekuens=(char*)malloc((strlen(token)) * sizeof(char));
+		strcpy((data+n)->sekuens,token);
 
-        (data+n)->panjang_sekuens=strlen(token);
+		(data+n)->panjang_sekuens=strlen(token);
 
-        token = strtok(NULL, ",");
-        (data+n)->nama_sampel = ...;
-        strcpy((data+n)->nama_sampel,token);
-    
-        n++; 
-    }fclose(seq_file);
+		token = strtok(NULL, ",");
+		(data+n)->nama_sampel=(char*)malloc((strlen(token)) * sizeof(char));
+		strcpy((data+n)->nama_sampel,token);
+	
+		n++; 
+	}fclose(seq_file);
 
-    *jml_sampel=n;
+	*jml_sampel=n;
 
-    return data;
+	return data;
 }
 
+int tiga_sekuens(Data_sampel* data, int n, char sekuens_ref[4]){
+	int sama=1;
+	int total_sama=0;
+	for(int i=0; i<(data+n)->panjang_sekuens; i++){
+		if((i+1)%3==1 && (data+n)->sekuens[i]!=sekuens_ref[0]){
+			sama=0;
+		}
+		else if((i+1)%3==2 && (data+n)->sekuens[i]!=sekuens_ref[1]){
+			sama=0;
+		}
+		else if((i+1)%3==0 && (data+n)->sekuens[i]!=sekuens_ref[2]){
+			sama=0;
+		}
+
+		if((i+1)%3==0){
+			if (sama==1){
+				total_sama++;
+			}
+			sama=1;
+		}
+	}
+	return total_sama;
+}
 
 int main(){
 	Data_sampel* data;
-    
-    data=...;
-	
-	//template input output
-    data=open_file(..., &...);
+	int jml_sampel, i, total_sama_per_sampel =0;
+	char sekuens_ref[4];
+
+	data=(Data_sampel*)malloc(sizeof(Data_sampel));
+
+	data=open_file(data, &jml_sampel);
 
 	printf("Masukkan 3 sekuens referensi: ");
-	scanf("%s", ...);
+	scanf("%s", &sekuens_ref);
 
 	printf("Nama sampel - Jumlah 3 sekuens yang sama dengan sekuens referensi\n");
-	printf("%s - %d\n", ..., ...);
+	for(i=0; i<jml_sampel; i++){
+		total_sama_per_sampel=tiga_sekuens(data, i, sekuens_ref);
+		printf("%s - %d\n", (data+i)->nama_sampel, total_sama_per_sampel);
+	}    
+
+	free(data);
 
 	return 0;
 }
